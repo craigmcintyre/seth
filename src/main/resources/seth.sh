fcda63ef4c0e02fea9c310301b7705123a787735
Craig McIntyre <craig.mcintyre@rapidsdata.com>
2017-05-31 11:00:32 +1000

    Adding a script file used for starting the SETH. It can be modified for different JDBC URLs and parameters. Tweaking what files get included in the uber jar.

---
+++ src/main/resources/seth.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Get the relative path to this file.
+DIR=$(dirname "$0")
+CLASSPATH='.:*:${DIR}:${DIR}/*'
+
+# The path to the se-shell
+SE_PATH="../OS X/Build/Products/Debug/SE"
+
+# Any other arguments to be provided to the se-shell in addition to
+# the --metadata and --script arguments which will automatically be added.
+SE_ARGS="--name NODE1 --sen ../Sen --commands ../Commands"  # in addition to --metadata and --script
+
+# Additional arguments for SETH. Enable as required.
+#SETH_ARGS="--clean --logtests"   # log all test executions to a file
+SETH_ARGS="--clean --logsteps"   # log executions of all steps within each test to a file.
+
+# The SE JDBC URL that tells the JDBC driver how to connect to SE.
+JDBC_URL="jdbc:rapidsse:${SE_PATH}?extraargs=${SE_ARGS}"
+
+# Run the SE Test Harness
+java -cp ${CLASSPATH} com.rapidsdata.seth.Seth -u "${JDBC_URL}" ${SETH_ARGS}  $@
+
