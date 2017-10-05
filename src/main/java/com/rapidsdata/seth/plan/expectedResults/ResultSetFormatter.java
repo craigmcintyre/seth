8161d8ebba575c810b0ecde4522e68e811914f84
Craig McIntyre <craig.mcintyre@rapidsdata.com>
2017-10-05 16:26:55 +1100

    Bug fix: not handling null values in the ResultSetFormatter. Also fixed some formatting discrepancies (not sure how they got in).

--- src/main/java/com/rapidsdata/seth/plan/expectedResults/ResultSetFormatter.java
+++ src/main/java/com/rapidsdata/seth/plan/expectedResults/ResultSetFormatter.java
@@ -22,99 +22,104 @@ public class ResultSetFormatter
     int numColumns = rsmd.getColumnCount();
 
     for (int colIndex = 1; colIndex <= numColumns; colIndex++) {
-     int columnType = rsmd.getColumnType(colIndex);
+      int columnType = rsmd.getColumnType(colIndex);
 
-     switch (columnType) {
+      // check for null first
+      if (rs.getObject(colIndex) == null) {
+        sb.append("null");
 
-       case Types.BIGINT:   // falls through
-       case Types.BIT:      // falls through
-       case Types.INTEGER:  // falls through
-       case Types.SMALLINT: // falls through
-       case Types.TINYINT:
-         sb.append(rs.getLong(colIndex));
-         break;
+      } else {
+        switch (columnType) {
 
-       case Types.BOOLEAN:
-         sb.append(rs.getBoolean(colIndex));
-         break;
+          case Types.BIGINT:   // falls through
+          case Types.BIT:      // falls through
+          case Types.INTEGER:  // falls through
+          case Types.SMALLINT: // falls through
+          case Types.TINYINT:
+            sb.append(rs.getLong(colIndex));
+            break;
 
-
-       case Types.CHAR:
-       case Types.VARCHAR:
-         sb.append('\'');
-         sb.append(rs.getString(colIndex).replace("'", "''"));
-         sb.append('\'');
-         break;
-
-       case Types.DATE:
-         sb.append("DATE '");
-         sb.append(rs.getDate(colIndex).toString());
-         sb.append("'");
-         break;
-
-       case Types.DECIMAL:
-       case Types.NUMERIC:
-         sb.append(rs.getBigDecimal(colIndex).toPlainString());
-         break;
-
-       case Types.DOUBLE:
-       case Types.FLOAT:
-       case Types.REAL:
-         String s = String.valueOf(rs.getDouble(colIndex));
-         sb.append(s);
-
-         // Give it an exponent if it doesn't have one.
-         if (!s.contains("e") && !s.contains("E")) {
-           sb.append("e0");
-         }
-         break;
-
-       case Types.JAVA_OBJECT:
-       case Types.OTHER:
-         sb.append(rs.getObject(colIndex).toString());
-         break;
-
-       case Types.NULL:
-         sb.append("null");
-         break;
-
-       case Types.TIME:
-       case Types.TIME_WITH_TIMEZONE:
-         sb.append("TIME '");
-         sb.append(rs.getTime(colIndex).toString());
-         sb.append("'");
-         break;
-
-       case Types.TIMESTAMP:
-       case Types.TIMESTAMP_WITH_TIMEZONE:
-         sb.append("TIMESTAMP '");
-         sb.append(rs.getTimestamp(colIndex).toString());
-         sb.append("'");
-         break;
+          case Types.BOOLEAN:
+            sb.append(rs.getBoolean(colIndex));
+            break;
 
 
+          case Types.CHAR:
+          case Types.VARCHAR:
+            sb.append('\'');
+            sb.append(rs.getString(colIndex).replace("'", "''"));
+            sb.append('\'');
+            break;
 
-       case Types.ARRAY:          // falls through
-       case Types.BINARY:         // falls through
-       case Types.BLOB:           // falls through
-       case Types.CLOB:           // falls through
-       case Types.DATALINK:       // falls through
-       case Types.DISTINCT:       // falls through
-       case Types.LONGNVARCHAR:   // falls through
-       case Types.LONGVARBINARY:  // falls through
-       case Types.LONGVARCHAR:    // falls through
-       case Types.NCHAR:          // falls through
-       case Types.NCLOB:          // falls through
-       case Types.NVARCHAR:       // falls through
-       case Types.REF:            // falls through
-       case Types.REF_CURSOR:     // falls through
-       case Types.ROWID:          // falls through
-       case Types.SQLXML:         // falls through
-       case Types.STRUCT:         // falls through
-       case Types.VARBINARY:      // falls through
-       default:
-         throw new SethSystemException("Unhandled JDBC column type: " + columnType);
-     }
+          case Types.DATE:
+            sb.append("DATE '");
+            sb.append(rs.getDate(colIndex).toString());
+            sb.append("'");
+            break;
+
+          case Types.DECIMAL:
+          case Types.NUMERIC:
+            sb.append(rs.getBigDecimal(colIndex).toPlainString());
+            break;
+
+          case Types.DOUBLE:
+          case Types.FLOAT:
+          case Types.REAL:
+            String s = String.valueOf(rs.getDouble(colIndex));
+            sb.append(s);
+
+            // Give it an exponent if it doesn't have one.
+            if (!s.contains("e") && !s.contains("E")) {
+              sb.append("e0");
+            }
+            break;
+
+          case Types.JAVA_OBJECT:
+          case Types.OTHER:
+            sb.append(rs.getObject(colIndex).toString());
+            break;
+
+          case Types.NULL:
+            sb.append("null");
+            break;
+
+          case Types.TIME:
+          case Types.TIME_WITH_TIMEZONE:
+            sb.append("TIME '");
+            sb.append(rs.getTime(colIndex).toString());
+            sb.append("'");
+            break;
+
+          case Types.TIMESTAMP:
+          case Types.TIMESTAMP_WITH_TIMEZONE:
+            sb.append("TIMESTAMP '");
+            sb.append(rs.getTimestamp(colIndex).toString());
+            sb.append("'");
+            break;
+
+
+          case Types.ARRAY:          // falls through
+          case Types.BINARY:         // falls through
+          case Types.BLOB:           // falls through
+          case Types.CLOB:           // falls through
+          case Types.DATALINK:       // falls through
+          case Types.DISTINCT:       // falls through
+          case Types.LONGNVARCHAR:   // falls through
+          case Types.LONGVARBINARY:  // falls through
+          case Types.LONGVARCHAR:    // falls through
+          case Types.NCHAR:          // falls through
+          case Types.NCLOB:          // falls through
+          case Types.NVARCHAR:       // falls through
+          case Types.REF:            // falls through
+          case Types.REF_CURSOR:     // falls through
+          case Types.ROWID:          // falls through
+          case Types.SQLXML:         // falls through
+          case Types.STRUCT:         // falls through
+          case Types.VARBINARY:      // falls through
+          default:
+            throw new SethSystemException("Unhandled JDBC column type: " + columnType);
+        }
+      }
 
       sb.append(", ");
     }
